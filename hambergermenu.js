1ee40c62ecfc04194fda346731831629122f8c5e
ritsuki-i <ritsuritsu143@gmail.com>
2023-07-02 23:39:15 +0900

    last commit

--- hambergermenu.js
+++ hambergermenu.js
@@ -1,8 +1,17 @@
 const ham = document.querySelector('#js-hamburger');
+const a_tag = document.querySelector('.nav__items');
 const nav = document.querySelector('#js-nav');
 const my_name = document.querySelector('#my-name');
 
 ham.addEventListener('click', function () {
+
+  ham.classList.toggle('active');
+  nav.classList.toggle('active');
+	my_name.classList.toggle('active');
+
+});
+
+a_tag.addEventListener('click', function () {
 
   ham.classList.toggle('active');
   nav.classList.toggle('active');
